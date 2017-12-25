2789d6c9f29f83b6f368103c88429c321058c850
Administrator <Administrator@192.168.1.110>
2017-12-25 15:04:08 +0800

    a

---
+++ chen-two/src/test/java/com/test/control/Test.java
@@ -0,0 +1,21 @@
+package com.test.control;
+
+import java.util.Date;
+import java.util.Hashtable;
+
+import org.apache.log4j.chainsaw.Main;
+
+public class Test {
+	
+	
+	
+	public static void main(String[] args) {
+		String a = "\\images\\";
+		String s = "D:\\data" + a;
+		System.out.println(s);
+		Date date = new Date();
+//		date.getHours()
+//		date.getMinutes()
+//		new Hashtable<K, V>();
+	}
+}
